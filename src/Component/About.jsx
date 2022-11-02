ce636031bab0b9b2a7627c3881633607ee2296e4
oadenekan <lillad25@yahoo.com>
2022-11-02 22:35:59 +0000

    updated nested route view

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import Governance from "./Governance";
+import Corporate from "./Corporate";
 
 const About = () => {
   return (
     <React.Fragment>
       <div>This is About page</div>
+      <li className="about-nested-wrapper">
+        <Link to="governance">Governance</Link >
+      </li>
+      <li className="about-nested-wrapper">
+        <Link to="corporate">Corporate</Link >
+      </li>
       <Outlet/>
     </React.Fragment>
   )
