c65c4d2af837be580f1b380b316fd0f3ebabb953
oadenekan <lillad25@yahoo.com>
2022-11-02 23:05:30 +0000

    remove eslint errors

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
-import Governance from "./Governance";
-import Corporate from "./Corporate";
 
 const About = () => {
   return (
